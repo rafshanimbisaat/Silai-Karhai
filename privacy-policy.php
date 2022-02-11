1dfc687042b1c7e04e78a5e81c1324bbcb2b7244
rafshanimbisaat <85075850+rafshanimbisaat@users.noreply.github.com>
2022-02-11 23:42:31 +0500

    Add files via upload

---
+++ privacy-policy.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+include "db.php";
+include("./layouts/header.php");
+?>
+
+    <section class="section section-bg" id="call-to-action" style="background-image: url(assets/images/p1.jpg)">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 offset-lg-1">
+                    <div class="cta-content">
+                        <br>
+                        <br>
+                        <h2>Privacy <em>Policy</em></h2>
+                        <h3><img src="assets/images/line-dec.png" alt=""></h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+ <section class="section" id="our-classes">
+        <div class="container">
+            <article>
+                <ol class="list-group">
+                    <li class="list-group-item"><h3><span class="font-weight-bold"><span>Privacy Policy</span></h3></li>
+                    <li class="list-group-item"> <span>Welcome to the Silai karhai website. We respect your privacy and want to protect your personal information. To learn more, please read this Privacy Policy. Our online privacy notice applies only to information collected through our website and not to information that may be collected offline.</span></li>
+    
+    <li class="list-group-item"> <span class="font-weight-bold"><span>Information We Collect</span></li>
+     <li class="list-group-item"> <span>Generally, you control the amount and type of information that you provide to us when using our website. When you visit our site, we automatically collect certain information about your device, including information about your web browser, IP address, time zone, and some of the cookies that are installed on your device. Additionally, as you browse the Site, we collect information about the individual web pages or products that you view, what websites or search terms referred you to the Site, and information about how you interact with the Site. We refer to this automatically-collected information as “Device Information.”</span></li>
+
+  <li class="list-group-item"> <span class="font-weight-bold"><span>We collect Device Information using the following technologies:</span></li>
+    <li class="list-group-item"> <span>1. “Cookies” are data files that are placed on your device or computer and often include an anonymous unique identifier. For more information about cookies, and how to disable cookies, visit http://www.allaboutcookies.org.</span></li>
+    <li class="list-group-item"> <span>2. “Log files” track actions occurring on our site, and collect data including your IP address, browser  type, Internet service provider, referring/exit pages, and date/time stamps.</span></li>
+    <li class="list-group-item"> <span>3. “Web beacons,” “tags,” and “pixels” are electronic files used to record information about how you browse our site.</span></li>
+    <li class="list-group-item"> <span>Additionally, when you make a purchase or attempt to make a purchase through our site, we collect certain information from you, including your name, billing address, shipping address, email address, and phone number.  We refer to this information as “Order Information.”</span></li>
+   <li class="list-group-item"> <span class="font-weight-bold"><span>How We Use Your Personal Information</span></li>
+    <li class="list-group-item"> <span>We use the Order Information that we collect generally to fulfill any orders placed through our site (including processing your payment information, arranging for shipping, and providing you with invoices and/or order confirmations). Additionally, we use this Order Information to:</span></li>
+    <li class="list-group-item"> <span>1. Communicate with you;</span></li>
+    <li class="list-group-item"> <span>2. Screen our orders for potential risk or fraud; and</span></li>
+    <li class="list-group-item"> <span>3. When in line with the preferences you have shared with us, provide you with information or advertising relating to our products or services.</span></li>
+
+    <li class="list-group-item"> <span>We use the Device Information that we collect to help us screen for potential risk and fraud (in particular, your IP address). We use Google Analytics to help us understand how our customers use our site; you can read more about how Google uses your Personal Information here: https://www.google.com/intl/en/policies/privacy/. You can also opt-out of Google Analytics here: https://tools.google.com/dlpage/gaoptout.</span></li>
+    <li class="list-group-item"> <span>Finally, we may also share your Personal Information to comply with applicable laws and regulations, to respond to a subpoena, search warrant or other lawful request for information we receive, or to otherwise protect our rights.</span></li>
+    <li class="list-group-item"> <span class="font-weight-bold"><span>Data Retention</span></li>
+    <li class="list-group-item"> <span>When you place an order through our site, we will maintain your Order Information for our records.</span></li>
+   <li class="list-group-item"> <span class="font-weight-bold"><span>Security Policy</span></li>
+     <li class="list-group-item"> <span>Our website is secured using the industry standard, Secure Sockets Layer (SSL) encryption. It encrypts all your personal information, so that it cannot be read over the internet. Unfortunately, we cannot guarantee against the loss or misuse of your personal data or secure data transmission over the internet because of its nature.</span></li>
+ <li class="list-group-item"> <span class="font-weight-bold"><span>Changes to Our Privacy Notice</span></li>
+     <li class="list-group-item"> <span>We reserve the right to change our privacy notice at any time. If our company decides to change this privacy notice, we will post those changes on this page so that our users are always up to date on what information we collect, use, and disclose.sYour continued use of our website, services, and products after any change to this privacy notice will constitute your acceptance of that change.</span></li>
+  <li class="list-group-item"> <span class="font-weight-bold"><span>According to our Policy</span></li>
+  <li class="list-group-item"> <span>> Order time will be 7 working days vendor should complete the order in 7 working days and deliverry time will be 4 to 5 working days. </span></li>
+            <li class="list-group-item"> <span>> Payment will be cash on the delivery customer should pay 75% payment to the rider while the rider picks up the stuff & the remaining 25% payment is paid when the rider delivers orders.</span></li>
+           <li class="list-group-item"> <span>> Customers should give authentic information while registration in case of wrong information order will be canceled.</span></li>
+            <li class="list-group-item"> <span>> Customer can check their order status through their order tracking id which is given to the customer when their order was placed.</span></li>
+            <li class="list-group-item"> <span>> Customers should give the right measurements of cloths to the vendor,  <li class="list-group-item"><span>> if their measurements are wronged, otherwise their order will be canceled.</span></li>
+     <li class="list-group-item"> <span>> The admin/owner will pay 240 rs per order to the rider. When rider deliver order to vendor from customer and when rider deliver order from vendor to customer 120 rs respectively</span></li>
+         <li class="list-group-item"> <span>> The admin will pay 70% per order to the vendor.</span></li>
+         <li class="list-group-item"> <span>> If rider will not deliver order according to time the he will get penalty of 100 rs on that order</span></li>
+          <li class="list-group-item"> <span class="font-weight-bold"><span>Questions About Our Privacy Notice</span></li>
+   <li class="list-group-item"> <span>If you have any questions about our privacy notice, please contact us via contact us form.</span></li>
+            </article>
+        </div>
+    </section>
+    <?php
+include("./layouts/footer.php")
+?>
